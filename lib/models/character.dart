9d94fd7a0652fb66e881cdaae15c7d27040406af
prime-infinity <soe.a.prime@gmail.com>
2024-10-10 02:49:13 -0700

    getters

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,4 +1,6 @@
-class Character {
+import 'package:flutter_rpg/models/stats.dart';
+
+class Character with Stats {
   //constructor
   Character({required this.name, required this.slogan, required this.id});
 
@@ -7,6 +9,9 @@ class Character {
   final String slogan;
   final String id;
   bool _isFav = false; //private field cannot be mutated from outside class
+
+  //getters
+  bool get isFav => _isFav;
 
   void toggleIsFav() {
     _isFav = !_isFav;
