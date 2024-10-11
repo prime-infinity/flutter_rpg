b1c5ce8146cc62ac91cc348ddfc036a5706adb02
prime-infinity <soe.a.prime@gmail.com>
2024-10-11 12:04:31 -0700

    adding characters to the charater class

--- lib/screens/create/create.dart
+++ lib/screens/create/create.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/character.dart';
 import 'package:flutter_rpg/models/vocation.dart';
 import 'package:flutter_rpg/screens/create/vocation_card.dart';
 import 'package:flutter_rpg/shared/styled_button.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:uuid/uuid.dart';
+
+var uuid = const Uuid();
 
 class Create extends StatefulWidget {
   const Create({super.key});
@@ -35,15 +39,20 @@ class _CreateState extends State<Create> {
   //submit handler
   void handleSubmit() {
     if (_nameController.text.trim().isEmpty) {
-      print("name must not be empty");
+      //show error dialogue
       return;
     }
     if (_sloganController.text.trim().isEmpty) {
-      print("slogan must not be empty");
+      //show error dialogue
       return;
     }
-    print(_nameController.text);
-    print(_sloganController.text);
+
+    characters.add(Character(
+      name: _nameController.text.trim(),
+      slogan: _sloganController.text.trim(),
+      vocation: selectedVocation,
+      id: uuid.v4(),
+    ));
   }
 
   @override
@@ -134,6 +143,16 @@ class _CreateState extends State<Create> {
                   onTap: updateVocation,
                   vocation: Vocation.wizard),
 
+              //goodluck message
+              const Center(
+                child: StyledHeading("Good Luck"),
+              ),
+              const Center(
+                child: StyledText("And enjoy the journey..."),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
               Center(
                 child: StyledButton(
                     onPressed: handleSubmit,
