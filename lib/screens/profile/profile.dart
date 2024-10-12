a2f32c11aa758e2377bcce5573ad36010c5aedb5
prime-infinity <soe.a.prime@gmail.com>
2024-10-12 13:28:16 -0700

    doing character profile screen

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rpg/models/character.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
+import 'package:flutter_rpg/theme.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key, required this.character});
@@ -11,13 +12,36 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const StyledTitle("character name"),
+        title: StyledTitle(character.name),
       ),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             //basic info - image, vocation, description
+            Container(
+              padding: const EdgeInsets.all(16),
+              color: AppColors.secondaryColor.withOpacity(0.3),
+              child: Row(children: [
+                Image.asset(
+                  'assets/img/vocations/${character.vocation.image}',
+                  width: 140,
+                  height: 140,
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      StyledHeading(character.vocation.title),
+                      StyledText(character.vocation.description)
+                    ],
+                  ),
+                )
+              ]),
+            ),
 
             //weapon and ability
 
