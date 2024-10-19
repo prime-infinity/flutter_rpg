4157d2a6f88f1049a488ec2205576ac7a1561da8
prime-infinity <soe.a.prime@gmail.com>
2024-10-19 02:19:23 -0700

    created character store

---
+++ lib/services/character_store.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/character.dart';
+import 'package:flutter_rpg/models/vocation.dart';
+
+class CharacterStore extends ChangeNotifier {
+  final List<Character> _characters = [
+    Character(
+        name: "Klara", slogan: "Kapumf!", vocation: Vocation.wizard, id: "1"),
+    Character(
+        id: '2',
+        name: 'Jonny',
+        vocation: Vocation.junkie,
+        slogan: 'Light me up...'),
+    Character(
+        id: '3',
+        name: 'Crimson',
+        vocation: Vocation.raider,
+        slogan: 'Fire in the hole!'),
+    Character(
+        id: '4',
+        name: 'Shaun',
+        vocation: Vocation.ninja,
+        slogan: 'Alright then gang.'),
+  ];
+
+  get characters => _characters;
+}
