e57e8d390dea892944758bc3451358295eb1488c
prime-infinity <soe.a.prime@gmail.com>
2024-10-21 09:13:29 -0700

    made factory converter

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_rpg/models/skill.dart';
 import 'package:flutter_rpg/models/stats.dart';
 import 'package:flutter_rpg/models/vocation.dart';
@@ -42,5 +43,34 @@ class Character with Stats {
       "stats": statsAsMap,
       "points": points,
     };
+  }
+
+  //character from firestore... here we use the factory
+  factory Character.fromFirestore(
+      DocumentSnapshot<Map<String, dynamic>> snapshot,
+      SnapshotOptions? options) {
+    //get data from snapshot
+    final data = snapshot.data()!;
+
+    //use data to make character instance
+    Character character = Character(
+        name: data["name"],
+        slogan: data['slogan'],
+        vocation:
+            Vocation.values.firstWhere((v) => v.toString() == data['vocation']),
+        id: snapshot.id);
+
+    //update skill
+    for (String id in data['skills']) {
+      Skill skill = allSkills.firstWhere((sk) => sk.id == id);
+      character.updateSkill(skill);
+    }
+
+    //set isFav
+    if (data['isFav'] == true) {
+      character.toggleIsFav();
+    }
+
+    return character;
   }
 }
