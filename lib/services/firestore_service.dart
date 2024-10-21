54b94642a0cc215bbd31603cb595dde002b0bf4a
prime-infinity <soe.a.prime@gmail.com>
2024-10-21 09:25:41 -0700

    firestore converters

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,5 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_rpg/models/character.dart';
 
 class FirestoreService {
-  static final ref = FirebaseFirestore.instance.collection("characters");
+  static final ref = FirebaseFirestore.instance
+      .collection("characters")
+      .withConverter(
+          fromFirestore: Character.fromFirestore,
+          toFirestore: (Character c, _) => c.toFirestore());
 }
