7c280013a7c3eb3aba7bf9c9e4d93c3dd5675193
prime-infinity <soe.a.prime@gmail.com>
2024-10-22 06:00:43 -0700

    created a tween sequence

--- lib/screens/profile/heart.dart
+++ lib/screens/profile/heart.dart
@@ -12,12 +12,17 @@ class Heart extends StatefulWidget {
 
 class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
+  //tween 25 -> 40
+  late Animation _sizeAnimation;
 
   @override
   void initState() {
     _controller = AnimationController(
         vsync: this, duration: const Duration(milliseconds: 200));
-
+    _sizeAnimation = TweenSequence([
+      TweenSequenceItem<double>(tween: Tween(begin: 25, end: 40), weight: 50),
+      TweenSequenceItem<double>(tween: Tween(begin: 40, end: 25), weight: 50)
+    ]).animate(_controller);
     super.initState();
   }
 
